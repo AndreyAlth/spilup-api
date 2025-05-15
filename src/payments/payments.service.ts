d8ab679f8457c7333bbffb203d0dd9b3096eac05
gabriel <andreyalth@gmail.com>
2025-05-15 15:50:00 -0600

    add service to create order conekta

--- src/payments/payments.service.ts
+++ src/payments/payments.service.ts
@@ -1,6 +1,14 @@
 import { Injectable } from '@nestjs/common'
 // import { Customer } from './dto/customer-payment.dto';
-import { Customer, Configuration, CustomersApi, CustomerResponse } from 'conekta'
+import {
+  Customer,
+  Configuration,
+  CustomersApi,
+  CustomerResponse,
+  OrdersApi,
+  OrderResponse,
+  OrderRequest,
+} from 'conekta'
 
 @Injectable()
 export class PaymentsService {
@@ -9,12 +17,25 @@ export class PaymentsService {
   conektaApiKey = process.env.CONEKTA_API_KEY
   config = new Configuration({ accessToken: this.conektaApiKey })
   client = new CustomersApi(this.config)
+  orderClient = new OrdersApi(this.config)
 
   async createCustomer(customer: Customer): Promise<CustomerResponse | undefined> {
     try {
       const response = await this.client.createCustomer(customer).then((response) => {
         const customerResponse = response.data
         return customerResponse
+      })
+      return response
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  async createOrder(order: OrderRequest): Promise<OrderResponse | undefined> {
+    try {
+      const response = await this.orderClient.createOrder(order).then((response) => {
+        const orderResponse = response.data
+        return orderResponse
       })
       return response
     } catch (error) {
