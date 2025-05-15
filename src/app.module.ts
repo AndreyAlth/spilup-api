fb7916f87089f6378e8b3819ba298b3eaee83702
gabriel <andreyalth@gmail.com>
2025-05-15 14:08:52 -0600

    add payment module

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,11 @@ import { AuthService } from './auth/auth.service';
 import { AuthModule } from './auth/auth.module';
 import { ApiKeyModule } from './api-key/api-key.module';
 import { TokensModule } from './tokens/tokens.module';
+import { PaymentsModule } from './payments/payments.module';
+import { PaymentsModule } from './payments/payments.module';
 
 @Module({
-  imports: [UsersModule, PrismaModule, AuthModule, ApiKeyModule, TokensModule],
+  imports: [UsersModule, PrismaModule, AuthModule, ApiKeyModule, TokensModule, PaymentsModule],
   controllers: [AuthController],
   providers: [AuthService],
 })
