5be02c7c6b9adb4ed4e006a01709042a54d4fbf4
gabriel <andreyalth@gmail.com>
2025-05-19 10:28:14 -0600

    add cors to app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { PrismaModule } from './prisma/prisma.module';
@@ -6,6 +7,17 @@ import { AuthService } from './auth/auth.service';
 import { AuthModule } from './auth/auth.module';
 import { ApiKeyModule } from './api-key/api-key.module';
 import { TokensModule } from './tokens/tokens.module';
+=======
+import { Module } from '@nestjs/common'
+import { UsersModule } from './users/users.module'
+import { PrismaModule } from './prisma/prisma.module'
+import { AuthController } from './auth/auth.controller'
+import { AuthService } from './auth/auth.service'
+import { AuthModule } from './auth/auth.module'
+import { ApiKeyModule } from './api-key/api-key.module'
+import { TokensModule } from './tokens/tokens.module'
+import { PaymentsModule } from './payments/payments.module'
+>>>>>>> Stashed changes
 
 @Module({
   imports: [UsersModule, PrismaModule, AuthModule, ApiKeyModule, TokensModule],
