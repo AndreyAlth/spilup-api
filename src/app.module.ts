8af74c441d9a26a2cdbef206622afe7b2a893f39
gabriel <andreyalth@gmail.com>
2025-05-19 10:35:59 -0600

    resolve conflict

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { PrismaModule } from './prisma/prisma.module';
@@ -7,17 +6,6 @@ import { AuthService } from './auth/auth.service';
 import { AuthModule } from './auth/auth.module';
 import { ApiKeyModule } from './api-key/api-key.module';
 import { TokensModule } from './tokens/tokens.module';
-=======
-import { Module } from '@nestjs/common'
-import { UsersModule } from './users/users.module'
-import { PrismaModule } from './prisma/prisma.module'
-import { AuthController } from './auth/auth.controller'
-import { AuthService } from './auth/auth.service'
-import { AuthModule } from './auth/auth.module'
-import { ApiKeyModule } from './api-key/api-key.module'
-import { TokensModule } from './tokens/tokens.module'
-import { PaymentsModule } from './payments/payments.module'
->>>>>>> Stashed changes
 
 @Module({
   imports: [UsersModule, PrismaModule, AuthModule, ApiKeyModule, TokensModule],
